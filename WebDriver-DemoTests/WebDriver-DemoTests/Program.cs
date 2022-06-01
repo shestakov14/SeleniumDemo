026fcc9ee9657fc2992f5dce402a632347a71bb7
Emil Shestakov <40560445+shestakov14@users.noreply.github.com>
2022-06-01 11:10:07 +0300

    Add files via upload

---
+++ WebDriver-DemoTests/WebDriver-DemoTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebDriver_DemoTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
